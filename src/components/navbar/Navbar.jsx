66188f54eb51ef7c442f4cdda839cbeec90c5454
shishir <mdabdulaziz3745@gmail.com>
2025-06-03 20:12:17 +0600

    added navbar and error page

---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { Link, NavLink } from "react-router";
+
+const Navbar = () => {
+  const links = (
+    <>
+      <li>
+        <NavLink to={"/"}>Home</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/marathon"}>Marathon</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/login"}>Login</NavLink>
+      </li>
+      <li>
+        <NavLink to={"/register"}>Register</NavLink>
+      </li>
+    </>
+  );
+  return (
+    <nav className="navbar bg-base-100 shadow-sm">
+      <div className="flex-1">
+        <Link className="btn btn-ghost text-xl">
+          Marathon Management System
+        </Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">{links}</ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
--- src/router.jsx
+++ src/router.jsx
@@ -1,9 +1,19 @@
 import { createBrowserRouter } from "react-router";
+import Root from "./rootLayout/Root";
+import Home from "./pages/home/Home";
+import ErrorPage from "./pages/errorPage/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    errorElement: <ErrorPage></ErrorPage>,
+    Component: Root,
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
   },
 ]);
 
