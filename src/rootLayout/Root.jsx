3e8400a35725aa997b69e88e37be18996c378412
shishir <mdabdulaziz3745@gmail.com>
2025-06-15 16:29:43 +0600

    added footer

--- src/rootLayout/Root.jsx
+++ src/rootLayout/Root.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from "react-router";
 import Navbar from "../components/navbar/Navbar";
+import Footer from "../components/footer/Footer";
 
 const Root = () => {
   return (
     <div>
       <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
